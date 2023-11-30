76606fa2c43b183ccb88df7a4b4db3bbe98421d7
Mohammad Qaiser Imam <qaisertech@gmail.com>
2023-11-30 18:45:44 +0530

    cakecandle

---
+++ birthdaycakecandles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        candles.Sort();
+        candles.Reverse();
+        var highestcandle = candles[0];
+        var numberofhighestcandle = candles.Count(c=>c == highestcandle);
+        Console.WriteLine(numberofhighestcandle);
+        return numberofhighestcandle;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+    }
+}
