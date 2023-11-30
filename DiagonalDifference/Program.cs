bd17282a410ebed3fe2d69f2f1b583b31809523a
qaiser imam <qaiser@comm-it.com.sa>
2023-11-30 13:23:35 +0530

    adding the warmpuop codes

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int firstDiagnoalValue = 0;
+        int secondDiagonalValue = 0;
+        int row = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            for (int j = 0; j < arr.Count; j++)
+            {
+                // Adding the first diagonal
+                if (i == j)
+                    firstDiagnoalValue += arr[i][j];
+                if (row == i && j == (arr.Count - 1))
+                {
+                    secondDiagonalValue += arr[i][j - i];
+                }
+            }
+            row++;
+        }
+        return Math.Abs(firstDiagnoalValue - secondDiagonalValue);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        Console.WriteLine("Enter the array length");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Enter the row number {i} value");
+
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        Console.WriteLine(result);
+        // textWriter.WriteLine(result);
+
+        // textWriter.Flush();
+        // textWriter.Close();
+    }
+}
---
+++ PlusMinus/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        float itemCount = arr.Count;
+        float positveCount = 0;
+        float negativeCount = 0;
+        float zeroCount = 0;
+        foreach (var item in arr)
+        {
+            if (item > 0)
+                positveCount++;
+            else if (item < 0)
+                negativeCount++;
+            else if (item == 0)
+                zeroCount++;
+        }
+        if (positveCount > 0)
+            Console.WriteLine((positveCount / itemCount).ToString("0.000000"));
+        else
+            Console.WriteLine(0.ToString("0.000000"));
+
+        if (negativeCount > 0)
+            Console.WriteLine((negativeCount / itemCount).ToString("0.000000"));
+        else
+            Console.WriteLine(0.ToString("0.000000"));
+
+        if (zeroCount > 0)
+            Console.WriteLine((zeroCount / itemCount).ToString("0.000000"));
+        else
+            Console.WriteLine(0.ToString("0.000000"));
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Add the number of arrays rows you want");
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
